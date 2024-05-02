0bbfc507a8dc32a6e341c49f31a29de4c15ff0e1
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-05-02 21:50:27 +0200

    Divided index.js into separately pieces

---
+++ show-balance.js
@@ -0,0 +1,40 @@
+window.onload = function () {
+  var chart = new CanvasJS.Chart("chartContainer", {
+    animationEnabled: true,
+    title: {
+      text: "",
+    },
+    data: [
+      {
+        type: "pie",
+        startAngle: 240,
+        yValueFormatString: '##0.00"PLN"',
+        indexLabel: "{label} {y}",
+        dataPoints: [
+          { y: 50.45, label: "Food" },
+          { y: 100.31, label: "House" },
+          { y: 150.06, label: "Transport" },
+          { y: 50.91, label: "Clothing" },
+          { y: 10.26, label: "Hygiene" },
+        ],
+      },
+    ],
+  });
+  chart.render();
+};
+
+document.getElementById("currentMonth").addEventListener("click", function () {
+  document.getElementById("changePeriod").innerHTML = "Current month";
+});
+
+document.getElementById("previousMonth").addEventListener("click", function () {
+  document.getElementById("changePeriod").innerHTML = "Previous month";
+});
+
+document.getElementById("currentYear").addEventListener("click", function () {
+  document.getElementById("changePeriod").innerHTML = "Current year";
+});
+
+document.getElementById("customPeriod").addEventListener("click", function () {
+  document.getElementById("changePeriod").innerHTML = "Custom period";
+});
